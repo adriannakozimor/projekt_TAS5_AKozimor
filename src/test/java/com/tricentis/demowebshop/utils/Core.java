5e7dfa70ebd3445ddb0dee0f355c8f8852eec1a9
Adrianna Kozimor <a.a.kozimor@gmail.com>
2026-05-19 21:11:41 +0200

    Remove unused createDriver method from Core class

--- src/test/java/com/tricentis/demowebshop/utils/Core.java
+++ src/test/java/com/tricentis/demowebshop/utils/Core.java
@@ -13,11 +13,6 @@ import java.nio.file.Path;
 public class Core {
     protected WebDriver driver;
 
-    protected WebDriver createDriver(String browser) {
-        driver = DriverFactory.createDriver(browser);
-        return driver;
-    }
-
     protected void quitDriver() {
         if (driver != null) {
             driver.quit();
